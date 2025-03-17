6c2083a55ebfbaab21e671f8299b2ad03fa200c6
Shasank Periwal <shasank.periwal@lumiq.ai>
2025-03-17 22:22:27 +0530

    Checkpoint- 1

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from langchain_pinecone import PineconeVectorStore
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
     print("Retrieval")
@@ -15,9 +20,9 @@ if __name__ == "__main__":
     llm = ChatGoogleGenerativeAI(temperature= 0, model="gemini-2.0-flash")
     
     query = "What is Vector?"
-    chain = PromptTemplate.from_template(template=query) | llm
-    result = chain.invoke(input={})
-    print(result.content)
+    # chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
     
     vectorstore = PineconeVectorStore(embedding= embeddings, index_name = os.environ['INDEX_NAME'])
 
@@ -26,4 +31,26 @@ if __name__ == "__main__":
     retrieval_chain = create_retrieval_chain(retriever=vectorstore.as_retriever(), combine_docs_chain = combine_docs_chain)
 
     result = retrieval_chain.invoke(input={"input": query})
-    print(result)
+    # print(result)
+
+    template = """Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer.
+
+    {context}
+
+    Question: {question}
+
+    Helpful Answer:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    # What is RunnablePassThrough()
+    rag_chain = (
+        {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
+
+    print(rag_chain.invoke(query).content)
